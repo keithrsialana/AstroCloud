691cdc520567550e57d6c8a927ddaa9463d4b06c
Keith Sialana <ninjabasketball@hotmail.com>
2024-10-20 22:30:50 -0400

    fixed line 21 in server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,14 @@
 import dotenv from 'dotenv';
+import path from 'path';
 import express from 'express';
 dotenv.config();
+
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+// Get the current file's URL and convert it to a file path
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 // Import the routes
 import routes from './routes/index.js';
@@ -10,7 +18,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Serve static files of entire client dist folder
-app.use(express.static('dist')); // points the server to the dist folder 
+app.use(express.static(path.join(__dirname,'../../client/dist'))); // points the server to the dist folder
 // Implement middleware for parsing JSON and urlencoded form data
 app.use(express.json()); // all data will be parsed in JSON format
 app.use(express.urlencoded({ extended: true })); // lets us use req.body in routes to grab data
